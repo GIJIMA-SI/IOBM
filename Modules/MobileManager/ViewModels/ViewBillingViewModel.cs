3a5df93348ffc473769c4bfae0fa3dcb71b76eab
10012434 <ferdi.hattingh@giji,a.com>
2017-03-22 09:46:46 +0200

    Ferdi

--- Modules/MobileManager/ViewModels/ViewBillingViewModel.cs
+++ Modules/MobileManager/ViewModels/ViewBillingViewModel.cs
@@ -13,7 +13,6 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace Gijima.IOBM.MobileManager.ViewModels
 {
