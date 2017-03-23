f1d98c46f1ff4245b68a1167cc3f2e243d06f5c6
Elmer Niehaus <elmerniehaus@gmail.com>
2017-03-23 11:50:15 +0200

    AdvancedSearch Complete V1.0

---
+++ Modules/MobileManager/Model/Gijima.IOBM.MobileManager.Model/Data/WhereCriteria.cs
@@ -0,0 +1,43 @@
+﻿using Gijima.IOBM.MobileManager.Model.Data;
+
+namespace Gijima.IOBM.MobileManager.Common.Structs
+{
+    public class WhereCriteria
+    {
+        /// <summary>
+        /// The AdvancedSearchField row
+        /// </summary>
+        public AdvancedSearchField SearchField
+        {
+            get { return _searchField; }
+            set { _searchField = value; }
+        }
+        private AdvancedSearchField _searchField;
+        /// <summary>
+        /// The operator value eg."PreFix"
+        /// </summary>
+        public string OperatorValue
+        {
+            get { return _operatorValue; }
+            set { _operatorValue = value; }
+        }
+        private string _operatorValue;
+        /// <summary>
+        /// The actual value the user entered
+        /// </summary>
+        public string SearchValue
+        {
+            get { return _searchValue; }
+            set { _searchValue = value; }
+        }
+        private string _searchValue;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public WhereCriteria()
+        {
+
+        }
+    }
+}
---
+++ Modules/MobileManager/Model/Gijima.IOBM.MobileManager.Model/Models/AdvancedSearchModel.cs
@@ -0,0 +1,84 @@
+﻿using Gijima.IOBM.Infrastructure.Events;
+using Gijima.IOBM.Infrastructure.Structs;
+using Gijima.IOBM.MobileManager.Model.Data;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace Gijima.IOBM.MobileManager.Model.Models
+{
+    public class AdvancedSearchModel
+    {
+        #region Properties and Attributes
+
+        private IEventAggregator _eventAggregator;
+
+        #endregion
+
+        /// <summary>
+        /// Constructure
+        /// </summary>
+        public AdvancedSearchModel(IEventAggregator eventAggreagator)
+        {
+            _eventAggregator = eventAggreagator;
+        }
+
+        /// <summary>
+        /// Returns the data from the given query
+        /// </summary>
+        /// <param name="sqlQuery"></param>
+        /// <returns></returns>
+        public DataTable ReadProvidedQuery(string sqlQuery)
+        {
+            try
+            {
+                string connectionString = MobileManagerEntities.GetContext().Database.Connection.ConnectionString;
+                
+                DataTable queryData = new DataTable();
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(queryData);
+                    }
+                    con.Close();
+                }
+
+                List<DataColumn> columnsToRemove = new List<DataColumn>();
+                //List all unwanted columns
+                foreach (DataColumn column in queryData.Columns)
+                {
+                    if (column.ColumnName.StartsWith("fk") || column.ColumnName.StartsWith("pk") ||
+                        column.ColumnName.StartsWith("en") || column.ColumnName == "IsActive" ||
+                        column.ColumnName == "ModifiedBy" || column.ColumnName == "ModifiedDate" || 
+                        column.ColumnName == "IsActive1" || column.ColumnName == "ModifiedBy1" || 
+                        column.ColumnName == "ModifiedDate1")
+                    { columnsToRemove.Add(column); }
+                }
+                //Remove all unwanted columns
+                foreach (DataColumn column in columnsToRemove)
+                {
+                    queryData.Columns.Remove(column);
+                }
+
+                return queryData;
+            }
+            catch (Exception ex)
+            {
+                _eventAggregator.GetEvent<ApplicationMessageEvent>()
+                                    .Publish(new ApplicationMessage(this.GetType().Name,
+                                             string.Format("Error! {0}, {1}.",
+                                             ex.Message, ex.InnerException != null ? ex.InnerException.Message : string.Empty),
+                                             MethodBase.GetCurrentMethod().Name,
+                                             ApplicationMessage.MessageTypes.SystemError));
+                return new DataTable();
+            }
+        }
+    }
+}
